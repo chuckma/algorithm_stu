a14aa1e03732811242b45903811d237953df781c
lucas ma <robbincen@163.com>
2018-05-29 19:56:02 +0800

    随机化 快速排序算法

--- src/cn/lucas/quickSort/QuickSort.java
+++ src/cn/lucas/quickSort/QuickSort.java
@@ -14,6 +14,9 @@ public class QuickSort {
     // 返回p, 使得arr[l...p-1] < arr[p] ; arr[p+1...r] > arr[p]
     private static int partition(Comparable[] arr, int l, int r){
 
+        // 随机在arr[l...r]的范围中, 选择一个数值作为标定点pivot
+        swap( arr, l , (int)(Math.random()*(r-l+1))+l );
+
         // 取当前数组的第一个元素
         Comparable v = arr[l];
 
@@ -32,9 +35,11 @@ public class QuickSort {
     // 递归使用快速排序,对arr[l...r]的范围进行排序
     private static void sort(Comparable[] arr, int l, int r){
 
-        if( l >= r )
+        // 对于小规模数组, 使用插入排序
+        if( r - l <= 15 ){
+            InsertionSort.sort(arr, l, r);
             return;
-
+        }
         int p = partition(arr, l, r);
         sort(arr, l, p-1 );
         sort(arr, p+1, r);
