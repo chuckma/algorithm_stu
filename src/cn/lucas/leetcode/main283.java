3b44adfebe2d961861635052026d180f6941f260
lucas ma <robbincen@163.com>
2018-07-08 21:43:31 +0800

    283 问题 优化

--- src/cn/lucas/leetcode/main283.java
+++ src/cn/lucas/leetcode/main283.java
@@ -37,11 +37,26 @@ public class main283 {
         }
     }
 
+    // 优化1
+    public static void moveZeroes1(int[] nums) {
+        int k = 0; // [0...k) 区间放置非 0 元素
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[k++] = nums[i];
+            }
+        }
+
+        // 将0放置在剩余的位置
+        for (int i = k; i < nums.length; i++) {
+            nums[i]=0;
+        }
+    }
 
     public static void main(String args[]){
 
         int[] arr = {0, 1, 0, 3, 12};
-        moveZeroes(arr);
+//        moveZeroes(arr);
+        moveZeroes1(arr);
         for(int i = 0 ; i < arr.length ; i ++)
             System.out.print(arr[i] + " ");
         System.out.println();
