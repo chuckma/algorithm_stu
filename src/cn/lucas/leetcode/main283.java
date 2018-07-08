ca10703f6a06b2f8864fe2da8c0973b100e18c7d
lucas ma <robbincen@163.com>
2018-07-08 21:53:37 +0800

    优化

--- src/cn/lucas/leetcode/main283.java
+++ src/cn/lucas/leetcode/main283.java
@@ -52,11 +52,28 @@ public class main283 {
         }
     }
 
+    public static void moveZeroes2(int[] nums) {
+        int k = 0; // [0...k) 区间放置非 0 元素
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                swap(nums, k++, i);
+            }
+        }
+
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
     public static void main(String args[]){
 
         int[] arr = {0, 1, 0, 3, 12};
 //        moveZeroes(arr);
-        moveZeroes1(arr);
+//        moveZeroes1(arr);
+        moveZeroes2(arr);
         for(int i = 0 ; i < arr.length ; i ++)
             System.out.print(arr[i] + " ");
         System.out.println();
